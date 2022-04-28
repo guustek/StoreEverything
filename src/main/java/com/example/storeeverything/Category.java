055d9b27b711adc18227f6f6ba494eeae48fc5d6
guustek <guustek@wp.pl>
2022-04-28 20:59:28 +0200

    Hibernate configuration

---
+++ src/main/java/com/example/storeeverything/Category.java
@@ -0,0 +1,46 @@
+package com.example.storeeverything;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Category {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "name", nullable = false, length = - 1)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
