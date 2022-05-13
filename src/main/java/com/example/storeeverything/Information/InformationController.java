a8166d7a885345011b5455eabf352f53231d6fe2
pi0trula <elo.piotrekk@gmail.com>
2022-05-13 13:44:38 +0200

    templates for InformationController

--- src/main/java/com/example/storeeverything/Information/InformationController.java
+++ src/main/java/com/example/storeeverything/Information/InformationController.java
@@ -1,66 +1,118 @@
 package com.example.storeeverything.Information;
 
+import com.example.storeeverything.Category.Category;
 import com.example.storeeverything.Category.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @CrossOrigin
-@RestController
+@Controller
 @RequestMapping("/informations")
 public class InformationController {
 
     private final InformationRepository informationRepository;
+    private final CategoryRepository categoryRepository;
 
     @Autowired
-    public InformationController(InformationRepository informationRepository) {
+    public InformationController(InformationRepository informationRepository, CategoryRepository categoryRepository) {
         this.informationRepository = informationRepository;
+        this.categoryRepository = categoryRepository;
+
     }
 
     @GetMapping("")
-    public ResponseEntity<List<Information>> getAll() {
-        return new ResponseEntity<>(informationRepository.findAll(), HttpStatus.OK);
+    public String getAll(Model model) {
+        List<Information> informations = informationRepository.findAll();
+        informations.sort(Comparator.comparing(Information :: getId));
+        model.addAttribute("informations", informations);
+        return "informations/informations";
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getById(@PathVariable int id) {
+    public String getById(@PathVariable int id, Model model) {
         try {
             Information information = informationRepository.findById(id).orElseThrow();
-            return new ResponseEntity<>(information, HttpStatus.OK);
+            model.addAttribute("information", information);
+            return "informations/details";
         } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(String.format("Information with Id=%d not found", id), HttpStatus.NOT_FOUND);
+            //return new ResponseEntity<>(String.format("Information with Id=%d not found", id), HttpStatus.NOT_FOUND);
+            /*
+             * TODO return if couldn't find information with this id
+             */
+            return "";
         }
     }
 
-    @PostMapping("")
-    public ResponseEntity<Object> add(@RequestBody Information data) {
-        data.setId(0);
-        if (! informationRepository.findAll().contains(data)) {
-            informationRepository.save(data);
-            return new ResponseEntity<>(data, HttpStatus.CREATED);
-        }
-        return new ResponseEntity<>(String.format("%s already exists", data), HttpStatus.CONFLICT);
+    @GetMapping("/add")
+    public String add(Model model) {
+        Information information = new Information();
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("information", information);
+        model.addAttribute("categories", categories);
+        return "informations/add";
     }
 
-    @DeleteMapping("/{id}")
-    ResponseEntity<Object> delete(@PathVariable int id) {
+    @PostMapping("/add")
+    public String add(@ModelAttribute("information") Information information) {
+        information.setId(0);
+        java.sql.Date sqlDate = new java.sql.Date(System.currentTimeMillis());
+        information.setAddedDate(sqlDate);
+        if (informationRepository.findAll().contains(information)) {
+            //return new ResponseEntity<>(String.format("%s already exists", information), HttpStatus.CONFLICT);
+            /*
+             * TODO return if found existing information
+             */
+            return "";
+        }
+        informationRepository.save(information);
+
+        return "redirect:/informations";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id) {
         try {
             Information information = informationRepository.findById(id).orElseThrow();
             informationRepository.deleteById(id);
-            return new ResponseEntity<>(information, HttpStatus.OK);
+            return "redirect:/informations";
         } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(String.format("Information with Id=%d not found", id), HttpStatus.NOT_FOUND);
+            //return new ResponseEntity<>(String.format("Information with Id=%d not found", id), HttpStatus.NOT_FOUND);
+            /*
+             * TODO return if couldn't find information with this id
+             */
+            return "";
         }
     }
 
-    @PutMapping("/{id}")
-    ResponseEntity<Information> put(@RequestBody Information data, @PathVariable int id) {
-        data.setId(id);
-        informationRepository.save(data);
-        return new ResponseEntity<>(data, HttpStatus.OK);
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        try {
+            Information information = informationRepository.findById(id).orElseThrow();
+            List<Category> categories = categoryRepository.findAll();
+            model.addAttribute("information", information);
+            model.addAttribute("categories", categories);
+            return "informations/edit";
+        } catch (NoSuchElementException e) {
+            /*
+             * TODO return if couldn't find category with this id
+             */
+            return "";
+        }
+    }
+
+    @PostMapping("/{id}/edit")
+    public String put(@ModelAttribute("information") Information information, @PathVariable int id) {
+        informationRepository.save(information);
+        return "redirect:/informations/" + id;
     }
 }
