675a2a58fed59a649a3baff346aed825b7bb7887
Patryk Klej <patryk.klej00@gmail.com>
2022-05-26 17:28:44 +0200

    Register with email ConfirmationToken

---
+++ src/main/java/com/example/storeeverything/Register/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.example.storeeverything.Register;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+@CrossOrigin
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
+    }
+}
---
+++ src/main/java/com/example/storeeverything/User/UserService.java
@@ -0,0 +1,58 @@
+package com.example.storeeverything.User;
+
+import com.example.storeeverything.Register.Token.ConfirmationToken;
+import com.example.storeeverything.Register.Token.ConfirmationTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository UserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final static String NOT_FOUND_MESSAGE = "user with email %s not found";
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return UserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(NOT_FOUND_MESSAGE,email)));
+    }
+
+    public String signUpUser(User user){
+        boolean userExists = UserRepository
+                .findByEmail(user.getEmail())
+                .isPresent();
+        if(userExists){
+            throw new IllegalStateException("email already taken");
+        }
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+
+        user.setPassword(encodedPassword);
+
+        UserRepository.save(user);
+
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationToken = new ConfirmationToken(token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                user
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return UserRepository.enableAppUser(email);
+    }
+}
