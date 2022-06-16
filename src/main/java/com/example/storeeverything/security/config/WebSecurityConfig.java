11ae722b6281be6c7353c8bead409f33a37e0d1a
Patryk Klej <patryk.klej00@gmail.com>
2022-06-16 23:06:11 +0200

    home page

--- src/main/java/com/example/storeeverything/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/storeeverything/security/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/css/**", "/js/**", "/webjars/**").permitAll()
+                .antMatchers("/css/**","/images/**", "/js/**", "/webjars/**").permitAll()
                 .antMatchers("/register/**", "/").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
