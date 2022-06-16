6cadbaba9238310e4ee01cd2b19997206e802f6e
Patryk Klej <patryk.klej00@gmail.com>
2022-06-16 19:39:56 +0200

    Admin panel controller

--- src/main/java/com/example/storeeverything/admin/AdminController.java
+++ src/main/java/com/example/storeeverything/admin/AdminController.java
@@ -1,8 +1,17 @@
 package com.example.storeeverything.admin;
 
+import com.example.storeeverything.category.Category;
+import com.example.storeeverything.information.Information;
+import com.example.storeeverything.user.User;
+import com.example.storeeverything.user.UserRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
@@ -10,12 +19,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class AdminController {
 
+    private final UserRepository userRepository;
+
+    public AdminController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("")
-    public String adminPanel(){
+    public String getAllUser(Model model) {
+        model.addAttribute("users", userRepository.findAll());
         return "admin/admin";
     }
 
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id) {
+        userRepository.deleteById(id);
+        return "redirect:/admin";
+    }
 
+    @PostMapping("/save")
+    public String put(@Valid @ModelAttribute("users") List<User> userList, Model model) {
+        userRepository.saveAll(userList);
+        return "redirect:/admin";
+
+    }
 
 
 }
