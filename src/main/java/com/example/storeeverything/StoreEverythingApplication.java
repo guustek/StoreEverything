6013f2bf6ea5de75749ef32ccda009128b1941cf
Paweł Augustyniak <guustek@wp.pl>
2022-06-20 15:59:49 +0200

    Thymeleaf date formatting

--- src/main/java/com/example/storeeverything/StoreEverythingApplication.java
+++ src/main/java/com/example/storeeverything/StoreEverythingApplication.java
@@ -2,12 +2,23 @@ package com.example.storeeverything;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class StoreEverythingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreEverythingApplication.class, args);
+    }
+
+    private ISpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addDialect(new Java8TimeDialect());
+        engine.setTemplateResolver(templateResolver);
+        return engine;
     }
 
 }
