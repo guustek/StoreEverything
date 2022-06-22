477b84fec4345b6b26d679a5f84e143ebd28c0fa
Paweł Augustyniak <guustek@wp.pl>
2022-06-22 03:05:20 +0200

    stream was collecting to an unmodifable list, and then it was modified (it was working anyway XD)

--- src/main/java/com/example/storeeverything/information/InformationController.java
+++ src/main/java/com/example/storeeverything/information/InformationController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
